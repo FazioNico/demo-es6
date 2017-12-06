f99a6cd153c2786e6b1f21bec5ae17fadeafea46
FazioNico <contact@nicolasfazio.ch>
2017-12-06 17:31:50 +0100

    upd

--- dev/app/pages/user/user.js
+++ dev/app/pages/user/user.js
@@ -22,7 +22,7 @@
      this.app.innerHTML = `
        <section>
          <h1 id="time"></h1>
-         <h2>Hello ${this.userData.email.split('@')[0]}</h2>
+         <h2>${this.greetings()} ${this.userData.email.split('@')[0]}</h2>
          <button id="logout">logout</button>
        </section>
      `;
@@ -35,5 +35,21 @@
      })
    }
 
-
+   greetings(){
+     let hour = new Date().getHours()
+     console.log(hour)
+     switch (true) {
+       case (hour > 18 && hour < 22):
+         return 'Good Evening'
+         break;
+       case (hour > 22 || hour < 6):
+         return 'Good Night'
+         break;
+       case (hour > 6 && hour < 12):
+         return 'Good morning'
+         break;
+       default:
+         return 'Hello'
+     }
+   }
  }
