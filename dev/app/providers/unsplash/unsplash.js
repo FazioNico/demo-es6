aa88e5f29dd249acede4dac0fcc161cbe4242edf
FazioNico <contact@nicolasfazio.ch>
2017-12-08 18:35:09 +0100

    replace XMLHTTPRequest by fetch()

--- dev/app/providers/unsplash/unsplash.js
+++ dev/app/providers/unsplash/unsplash.js
@@ -8,30 +8,35 @@ export class UnsplashProvider{
   }
 
   getRandomImg(){
-      //Return a new promise.
-      return new Promise((resolve, reject)=> {
-        // Do the usual XHR stuff
-        var req = new XMLHttpRequest();
-        req.open('GET', this.queryUrl+this.params.client_id);
-        req.onload = () =>{
-          // This is called even on 404 etc
-          // so check the status
-          if (req.status == 200) {
-            // Resolve the promise with the response text
-            resolve(JSON.parse(req.responseText));
-          }
-          else {
-            // Otherwise reject with the status text
-            // which will hopefully be a meaningful error
-            reject(Error(req.statusText));
-          }
-        };
-        // Handle network errors
-        req.onerror = ()=> {
-          reject(Error("Network Error"));
-        };
-        // Make the request
-        req.send();
-      });
+    return fetch(this.queryUrl+this.params.client_id, {
+      method: "GET"
+    })
+    .then(res=> res.json())
+    .catch(err => alert(err.toString()))
+      // //Return a new promise.
+      // return new Promise((resolve, reject)=> {
+      //   // Do the usual XHR stuff
+      //   var req = new XMLHttpRequest();
+      //   req.open('GET', this.queryUrl+this.params.client_id);
+      //   req.onload = () =>{
+      //     // This is called even on 404 etc
+      //     // so check the status
+      //     if (req.status == 200) {
+      //       // Resolve the promise with the response text
+      //       resolve(JSON.parse(req.responseText));
+      //     }
+      //     else {
+      //       // Otherwise reject with the status text
+      //       // which will hopefully be a meaningful error
+      //       reject(Error(req.statusText));
+      //     }
+      //   };
+      //   // Handle network errors
+      //   req.onerror = ()=> {
+      //     reject(Error("Network Error"));
+      //   };
+      //   // Make the request
+      //   req.send();
+      // });
   }
 }
